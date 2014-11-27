c71ebb915b504f391df9953bd9ebd3ad6b008535
Alexis Joubert <alexisjoubert@groupemsi.com>
2014-11-27 11:00:27 -0500

    fix

--- Controller/OrderController.php
+++ Controller/OrderController.php
@@ -4,7 +4,7 @@ namespace Kek\ShopBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\Validator\Constraints;
 
 class OrderController extends Controller
@@ -14,6 +14,10 @@ class OrderController extends Controller
      */
     public function historyAction()
     {
+        if (!$this->getUser()) {
+            throw new AccessDeniedException;
+        }
+
         $class = $this->container->getParameter('kek_shop.order.class');
         $repository = $this->getDoctrine()->getRepository($class);
         $orders = $repository->findBy(
